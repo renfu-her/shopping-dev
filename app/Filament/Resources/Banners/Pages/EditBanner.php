c854f2a99ac3767de29cb27741109b5bcf147d86
Patrick Her <renfu.her@gmail.com>
2025-09-10 16:29:09 +0800

    為橫幅表單新增重定向功能，確保在創建和編輯後能正確返回索引頁面，提升使用者體驗。

--- app/Filament/Resources/Banners/Pages/EditBanner.php
+++ app/Filament/Resources/Banners/Pages/EditBanner.php
@@ -16,4 +16,9 @@ class EditBanner extends EditRecord
             DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
